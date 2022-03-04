9964ac7b81322b4fa86eae18ab06ab16b912bf92
Nitaainun <nitaainun@student.telkomuniversity.ac.id>
2022-03-04 16:57:30 +0700

    git push

---
+++ TP MOD2/TP MOD2/Form1.cs
@@ -0,0 +1,36 @@
+namespace TP_MOD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            MessageBox.Show("Halo " + nama + "\n Semangat " + nama + "Kamu pasti bisa ^ ^");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
